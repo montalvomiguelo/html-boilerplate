f4da545b0ad499ab751cbcc897c4b1f21391b42a
Miguel Montalvo <miguel.montalvo@barrelny.com>
2023-06-07 21:54:40 -0700

    Load lit from CDN

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 import { counter, incCounter } from 'lib'
-incCounter()
-console.log(counter)
+import { LitElement, html } from 'lit'
+
+export class Counter extends LitElement {
+  constructor () {
+    super()
+    incCounter()
+    console.log(counter)
+  }
+
+  render () {
+    return html`<div>${counter}</div>`
+  }
+}
+customElements.define('my-counter', Counter)
